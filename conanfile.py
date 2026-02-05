5ff3acacd3e5dc0afee8f892b23214aab54f14d0
rodyherrera <contact@rodyherrera.com>
2026-02-05 04:51:09 -0300

    feat: displacements analysis algorithm

---
+++ conanfile.py
@@ -0,0 +1,45 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+
+
+class DisplacementAnalysisConan(ConanFile):
+    name = "displacement-analysis"
+    version = "1.0.0"
+    package_type = "static-library"
+    license = "MIT"
+    settings = "os", "arch", "compiler", "build_type"
+    requires = (
+        "coretoolkit/1.0.0",
+        "spdlog/1.14.1",  # Needed for linking
+        "fmt/10.2.1",  # Needed for linking (spdlog uses external fmt)
+        "nlohmann_json/3.11.3",  # Needed for json_utils.h
+    )
+    exports_sources = "CMakeLists.txt", "include/*", "src/*"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        CMakeToolchain(self).generate()
+        CMakeDeps(self).generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.set_property(
+            "cmake_target_name", "displacement-analysis::displacement-analysis"
+        )
+        self.cpp_info.libs = ["displacement-analysis_lib"]
+        self.cpp_info.requires = [
+            "coretoolkit::coretoolkit",
+            "nlohmann_json::nlohmann_json",
+            "spdlog::spdlog",
+            "fmt::fmt",
+        ]
