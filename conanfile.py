d0d6f1584b8c72badb224c89022a574a3395c3dc
rodyherrera <contact@rodyherrera.com>
2026-06-16 02:10:29 -0400

    feat: update version to 2.0.2 in conanfile.py, plugin.json, and vpm.json; adjust coretoolkit requirement

--- conanfile.py
+++ conanfile.py
@@ -4,14 +4,14 @@ from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 
 class DisplacementAnalysisConan(ConanFile):
     name = "displacement-analysis"
-    version = "2.0.1"
+    version = "2.0.2"
     package_type = "static-library"
     license = "MIT"
     settings = "os", "arch", "compiler", "build_type"
     requires = (
         "boost/1.88.0",
         "onetbb/2021.12.0",
-        "coretoolkit/[>=2.0]",
+        "coretoolkit/[>=2.5]",
         "spdlog/1.14.1",
         "nlohmann_json/3.11.3",
     )
